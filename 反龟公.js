b62de53c70c741aed3fd8f86c5453c3efb82ad2a
aiaimimi0920 <153103332+aiaimimi0920@users.noreply.github.com>
2024-10-25 20:44:28 +0800

    # 更新版本

--- 反龟公.js
+++ 反龟公.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         反龟公
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Display a huge red cross in the center of the screen on a specific webpage until navigating away
 // @author       mimi
 // @match        *://*/*
